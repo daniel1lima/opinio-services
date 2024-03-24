40291060363d60a1ee8090cc4e650d9390fcc8bf
Daniel Lima <damorosolima@gmail.com>
2024-03-24 02:35:31 -0700

    changes

--- flask_script.py
+++ flask_script.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 from yelp_script import yelp_script
 from gptscript import split_reviews
-from fitscorecalculator import calculate_all, calculate_fit_score
+# from fitscorecalculator import calculate_all, calculate_fit_score
 from mongofeeder import push_document
 
 filepath = 'DATA/sentiment_reviews_withcount.csv'
@@ -16,37 +16,18 @@ app = Flask(__name__)
 @app.route('/sentiment', methods=['GET'])
 #need to match above with axios call to /run-script
 def run_script():
-    # Run the Python scripts if we want
-    print("yelp script")
-    yelp_script("Gyms", 5)
+    # # Run the Python scripts if we want
+    # print("yelp script")
+    # yelp_script("Gyms", 5)
 
-    print("splitting reviews")
-    split_reviews()
+    # print("splitting reviews")
+    # split_reviews()
 
-    print("calculating all")
-    calculate_all()
+    # print("calculating all")
+    # calculate_all()
 
-
-    filepath = 'DATA/sentiment_reviews_withcount.csv'
-
-    df = pd.read_csv(filepath)
-    # Just some code that was in the calculate function
-    fit_scores_df = calculate_fit_score(df)
-    df = df.merge(fit_scores_df, on='name', how='left')
-    df_updated = pd.read_csv(filepath)
-
-    if 'FitScore_x' in df_updated.columns:
-        df_updated = df_updated.drop(columns=['FitScore_x'])
-
-    if 'review_count_rating' in df_updated.columns:
-        df_updated = df_updated.drop(columns=['review_count_rating'])
-
-             
-    df_updated.to_csv(filepath, index=False)
-    print("Updated CSV with FitScores.")
-
-    print("pushing")
-    push_document("DATA/sentiment_reviews_withcount.csv", "sentiment_reviews")
+    # print("pushing")
+    # push_document("DATA/sentiment_reviews_withcount.csv", "sentiment_reviews")
     
 
 
