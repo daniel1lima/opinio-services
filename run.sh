1f4709b157f49439866946219df30271d7f3746f
Daniel Lima <damorosolima@gmail.com>
2024-08-11 21:20:50 -0300

    changes for gunicorn and nginx

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+export PYTHONPATH=$(pwd)
+export FLASK_APP=./application.py
+
 # Define the application module
 APP_MODULE="application:app"
 
 # Number of workers
-NUM_WORKERS=4
+# NUM_WORKERS=4
 
 # Run Gunicorn with the specified number of workers and append output to nohup.out
-nohup gunicorn --workers $NUM_WORKERS $APP_MODULE >> nohup.out 2>&1 &
+nohup gunicorn --bind 0.0.0.0:5000 $APP_MODULE >> nohup.out
